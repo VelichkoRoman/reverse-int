d14515d0ba655b520584a0341803d26e10b2a12d
velichkoroman <tehvel@mail.ru>
2021-02-28 00:02:37 +0300

    add: add my ver reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n<0) {
+      n=n*(-1);}
+    let str=n.toString();
+    let result=str.split('').reverse().join('');
+    
+    return Number(result);
+  }
+
